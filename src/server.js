c860399a87a197928fa4deba48e801163c45653a
Lucas Garicoix <lucasgaricoix@hotmail.com>
2019-08-27 10:43:19 -0300

    Conectando com socket.io

--- src/server.js
+++ src/server.js
@@ -4,13 +4,28 @@ const cors = require('cors')
 const routes = require('./routes')
 
 const server = express();
+const serverSide = require('http').Server(server);
+const io = require('socket.io')(serverSide);
+
+server.use(cors());
+
+io.on('connection', socket => {
+  socket.on('connectRoom', room => {
+    socket.join(room);
+  })
+})
 
 mongoose.connect('mongodb+srv://lgaricoix:010133@cluster0-ndgtd.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
+server.use((req, res, next) => {
+  req.io = io;
+  return next();
+})
+
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 server.use(routes);
 
 server.listen(process.env.PORT || '3333');
