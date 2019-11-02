0be80f8c8a4ad11cd5bb3678f249a3d838aa070c
lucasgaricoix <lucasgaricoix@hotmail.com>
2019-11-01 21:30:00 -0300

    Deletando controladores não utilizados

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const PersonController = require('./controllers/PersonController');
 const BookController = require('./controllers/BookController')
 
 const routes = express.Router();
 
 //POST
-routes.post('/person', PersonController.store)
 routes.post('/book', BookController.store)
 
 //GET
-routes.get('/person/:id', PersonController.index)
 routes.get('/book/:bookingId', BookController.findBookById)
 routes.get('/book', BookController.findAll)
 
