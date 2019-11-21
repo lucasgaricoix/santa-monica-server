1dc136c56e944463d9d61d74dd2798c9aa01be9e
lucasgaricoix <lucasgaricoix@hotmail.com>
2019-11-21 01:42:01 -0300

    Adicionando endpoint para PUT

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -1,28 +1,48 @@
-const Book = require('../model/Book');
+const Book = require("../model/Book");
 
 module.exports = {
   async findBookById(req, res) {
     const book = await Book.findById(req.params.bookingId);
-    
+
     if (book) {
       return res.json(book);
     } else {
-      return res.status(400).json({ 
+      return res.status(400).json({
         error: `Booking with id: ${req.params.bookingId}, not found.`
-      })
+      });
     }
   },
 
   async store(req, res) {
+    const book = await Book.create(req.body);
 
-    const booking = await Book.create(req.body);
-
-    return res.json(booking);
+    return res.json(book);
   },
 
   async findAll(req, res) {
-
     const allBook = await Book.find();
+
     return res.json(allBook);
+  },
+
+  async update(req, res) {
+    const book = await Book.findById(req.params.id);
+
+    if (book) {
+      book.name = req.body.name;
+      book.email = req.body.email;
+      book.phoneNumber = req.body.phoneNumber;
+      book.bookDate = req.body.bookDate;
+      book.coolMessage = req.body.coolMessage;
+      book.isConfirmed = req.body.isConfirmed;
+
+      const updatedBook = await Book.create(book);
+
+      return res.json(updatedBook);
+    } else {
+      return res.status(400).json({
+        error: `Booking with id: ${req.params.bookingId}, not found.`
+      });
+    }
   }
-}
+};
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const BookController = require('./controllers/BookController')
+const express = require("express");
+const BookController = require("./controllers/BookController");
 
 const routes = express.Router();
 
 //POST
-routes.post('/book', BookController.store)
+routes.post("/book", BookController.store);
 
 //GET
-routes.get('/book/:bookingId', BookController.findBookById)
-routes.get('/book', BookController.findAll)
+routes.get("/book/:bookingId", BookController.findBookById);
+routes.get("/book", BookController.findAll);
+
+//PUT
+routes.put("/book/:id", BookController.update);
 
 module.exports = routes;
